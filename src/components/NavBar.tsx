d2420bd199242fa6340a01464f36b8cdfbc5da09
Peter Kerfoot <peterckerfoot@gmail.com>
2026-05-06 14:26:37 -0500

    feat: added simple navbar animation

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,8 @@
 const NavBar = () => {
   return (
-    <nav className='px-5'>
+    <nav className='px-5 animate-nav-in'>
       <ul>
-        <li className='py-3 pb-8 font-medium text-6xl text-gray-500 hover:text-gray-800 hover:cursor-pointer'>
+        <li className='py-3 pb-8 font-semibold text-5xl text-gray-500 tracking-tight hover:text-gray-800 hover:cursor-pointer'>
           <span className='text-yellow-500 hover:text-yellow-600'>Cecilia</span>{' '}
           Iwata
         </li>
