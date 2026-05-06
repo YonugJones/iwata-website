46d7c4c95eb19b0aa86326648c3a22a36a872a4e
Peter Kerfoot <peterckerfoot@gmail.com>
2026-05-06 10:58:24 -0500

    configure BrowserRouter layout w/ App route shell

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
-const App = () => {
-  return <div>App</div>
-}
+// Layout
 
+import { Outlet } from 'react-router-dom'
+import NavBar from './components/NavBar'
+
+const App = () => {
+  return (
+    <>
+      <nav>
+        <NavBar />
+      </nav>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  )
+}
 export default App
