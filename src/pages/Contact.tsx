0f44916a5e31faf29b1e5960ff6496bee6f19162
Peter Kerfoot <peterckerfoot@gmail.com>
2026-05-07 15:05:47 -0500

    revert fade in effect with tailwind @keyframes

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,42 +1,38 @@
-import PageTransition from '../components/PageTransition'
-
 const Contact = () => {
   return (
-    <PageTransition>
-      <div className='flex flex-col gap-4 max-w-2xl w-full mr-[19%]'>
-        <h2 className='text-3xl tracking-tight text-gray-500'>Get in touch</h2>
-        <form className='space-y-4 w-full'>
-          <div className='flex gap-4'>
-            <input
-              type='text'
-              placeholder='Name'
-              required
-              className='border border-gray-300 bg-gray-50 focus:bg-white focus:outline-none focus:ring-2 focus:ring-yellow-400 rounded-md p-2 w-full'
-            />
-            <input
-              type='email'
-              placeholder='Email'
-              required
-              className='border border-gray-300 bg-gray-50 focus:bg-white focus:outline-none focus:ring-2 focus:ring-yellow-400 rounded-md p-2 w-full'
-            />
-          </div>
-          <textarea
-            name='message'
-            rows={5}
-            id='message'
-            placeholder="What's up?"
+    <div className='flex flex-col gap-4 max-w-2xl w-full mr-[19% animate-fade-in'>
+      <h2 className='text-3xl tracking-tight text-gray-500'>Get in touch</h2>
+      <form className='space-y-4 w-full'>
+        <div className='flex gap-4'>
+          <input
+            type='text'
+            placeholder='Name'
             required
-            className='border border-gray-300 bg-gray-50 focus:bg-white focus:outline-none focus:ring-2 focus:ring-yellow-400 rounded-md p-2 w-full resize-none'
+            className='border border-gray-300 bg-gray-50 focus:bg-white focus:outline-none focus:ring-2 focus:ring-yellow-400 rounded-md p-2 w-full'
           />
-          <button
-            type='submit'
-            className='w-full bg-yellow-500 hover:bg-yellow-600 text-white font-medium px-6 py-2 rounded-md hover:cursor-pointer transition-colors duration-200'
-          >
-            Submit
-          </button>
-        </form>
-      </div>
-    </PageTransition>
+          <input
+            type='email'
+            placeholder='Email'
+            required
+            className='border border-gray-300 bg-gray-50 focus:bg-white focus:outline-none focus:ring-2 focus:ring-yellow-400 rounded-md p-2 w-full'
+          />
+        </div>
+        <textarea
+          name='message'
+          rows={5}
+          id='message'
+          placeholder="What's up?"
+          required
+          className='border border-gray-300 bg-gray-50 focus:bg-white focus:outline-none focus:ring-2 focus:ring-yellow-400 rounded-md p-2 w-full resize-none'
+        />
+        <button
+          type='submit'
+          className='w-full bg-yellow-500 hover:bg-yellow-600 text-white font-medium px-6 py-2 rounded-md hover:cursor-pointer transition-colors duration-200'
+        >
+          Submit
+        </button>
+      </form>
+    </div>
   )
 }
 
