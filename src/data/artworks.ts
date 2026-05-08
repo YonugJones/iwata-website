80993a7fe088adc96d04b5be48fe2b3b2147d943
Peter Kerfoot <peterckerfoot@gmail.com>
2026-05-08 18:21:58 -0500

    feat: add landing page with artwork carousel and artworks data structure

---
+++ src/data/artworks.ts
@@ -0,0 +1,112 @@
+type Artwork = {
+  id: string
+  category: 'sculpture' | 'painting'
+  src: string
+  title: string
+  year: number
+  materials?: string
+  dimensions?: string
+  description?: string
+}
+
+export const artworks: Artwork[] = [
+  {
+    id: '1',
+    category: 'sculpture',
+    src: 'https://picsum.photos/seed/1/900/600',
+    title: 'Placeholder Image 1',
+    year: 2000,
+    materials: 'Flour1, sugar1, egg1, milk1',
+    dimensions: '800 x 600',
+    description:
+      'This is a placeholder image1 that is dwarved in comparison to anything that Cecilia Iwata creates.',
+  },
+  {
+    id: '2',
+    category: 'sculpture',
+    src: 'https://picsum.photos/seed/2/900/600',
+    title: 'Placeholder Image 2',
+    year: 2000,
+    materials: 'Flour2, sugar2, egg2, milk2',
+    dimensions: '800 x 600',
+    description:
+      'This is a placeholder image2 that is dwarved in comparison to anything that Cecilia Iwata creates.',
+  },
+  {
+    id: '3',
+    category: 'sculpture',
+    src: 'https://picsum.photos/seed/3/900/600',
+    title: 'Placeholder Image 3',
+    year: 2000,
+    materials: 'Flour3, sugar3, egg3, milk3',
+    dimensions: '800 x 600',
+    description:
+      'This is a placeholder image3 that is dwarved in comparison to anything that Cecilia Iwata creates.',
+  },
+  {
+    id: '4',
+    category: 'sculpture',
+    src: 'https://picsum.photos/seed/4/900/600',
+    title: 'Placeholder Image 4',
+    year: 2000,
+    materials: 'Flour4, sugar4, egg4, milk4',
+    dimensions: '800 x 600',
+    description:
+      'This is a placeholder image4 that is dwarved in comparison to anything that Cecilia Iwata creates.',
+  },
+  {
+    id: '5',
+    category: 'sculpture',
+    src: 'https://picsum.photos/seed/5/900/600',
+    title: 'Placeholder Image 5',
+    year: 2000,
+    materials: 'Flour5, sugar5, egg5, milk5',
+    dimensions: '800 x 600',
+    description:
+      'This is a placeholder image5 that is dwarved in comparison to anything that Cecilia Iwata creates.',
+  },
+  {
+    id: '6',
+    category: 'sculpture',
+    src: 'https://picsum.photos/seed/6/900/600',
+    title: 'Placeholder Image 6',
+    year: 2000,
+    materials: 'Flour6, sugar6, egg6, milk6',
+    dimensions: '800 x 600',
+    description:
+      'This is a placeholder image6 that is dwarved in comparison to anything that Cecilia Iwata creates.',
+  },
+  {
+    id: '7',
+    category: 'sculpture',
+    src: 'https://picsum.photos/seed/7/900/600',
+    title: 'Placeholder Image 7',
+    year: 2000,
+    materials: 'Flour7, sugar7, egg7, milk7',
+    dimensions: '800 x 600',
+    description:
+      'This is a placeholder image7 that is dwarved in comparison to anything that Cecilia Iwata creates.',
+  },
+  {
+    id: '8',
+    category: 'sculpture',
+    src: 'https://picsum.photos/seed/8/900/600',
+    title: 'Placeholder Image 8',
+    year: 2000,
+    materials: 'Flour8, sugar8, egg8, milk8',
+    dimensions: '800 x 600',
+    description:
+      'This is a placeholder image8 that is dwarved in comparison to anything that Cecilia Iwata creates.',
+  },
+  {
+    id: '9',
+    category: 'sculpture',
+    src: 'https://picsum.photos/seed/9/900/600',
+    title: 'Placeholder Image 9',
+    year: 2000,
+    materials: 'Flour9, sugar9, egg9, milk9',
+    dimensions: '800 x 600',
+    description:
+      'This is a placeholder image9 that is dwarved in comparison to anything that Cecilia Iwata creates.',
+  },
+]
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,5 +1,47 @@
+import { useState } from 'react'
+import { artworks } from '../data/artworks'
+import { FaArrowRightLong, FaArrowLeftLong } from 'react-icons/fa6'
+
 const Landing = () => {
-  return <div>Landing</div>
+  const [index, setIndex] = useState(0)
+  const current = artworks[index]
+  const lastArtworkIndex = artworks.length - 1
+
+  const nextArtwork = () => {
+    if (index !== lastArtworkIndex) {
+      setIndex((prevIndex) => prevIndex + 1)
+    } else {
+      setIndex(0)
+    }
+    console.log('nextArtwork clicked!', `Artwork index ${index}`)
+  }
+
+  const prevArtwork = () => {
+    if (index !== 0) {
+      setIndex((prevIndex) => prevIndex - 1)
+    } else {
+      setIndex(lastArtworkIndex)
+    }
+    console.log('prevArtwork clicked!', `Artwork index ${index}`)
+  }
+
+  return (
+    <div className='animate-fade-in'>
+      <img src={current.src} alt={current.title} className='shadow-xl' />
+      <div className='flex justify-center items-center gap-6 py-8'>
+        <button className='hover:cursor-pointer' onClick={prevArtwork}>
+          <span className='inline-block transition-transform duration-500 hover:-translate-x-0.5'>
+            <FaArrowLeftLong className='text-gray-500 hover:text-gray-800 transition-colors duration-300' />
+          </span>
+        </button>
+        <button className='hover:cursor-pointer' onClick={nextArtwork}>
+          <span className='inline-block transition-transform duration-500 hover:translate-x-0.5'>
+            <FaArrowRightLong className='text-gray-500 hover:text-gray-800 transition-colors duration-300' />
+          </span>
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default Landing
