1fbf3454a7cf03d032b8bbca7653d55b2595356a
Peter Kerfoot <peterckerfoot@gmail.com>
2026-05-08 18:29:01 -0500

    style: add one shade darker gray paragraph font

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,7 +2,7 @@ import aboutImg from '../assets/cecilia-about-photo.jpg'
 
 const About = () => {
   return (
-    <div className='text-gray-400 animate-fade-in'>
+    <div className='text-gray-500 animate-fade-in'>
       <h1 className='text-4xl text-start font-semibold pb-4'>About</h1>
       <div className='grid grid-cols-2 gap-6 text-sm'>
         <div className='max-w-md'>
