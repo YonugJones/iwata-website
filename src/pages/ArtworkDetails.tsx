268799b36b662f43f6bed47b156d1f639fd1a414
Peter Kerfoot <peterckerfoot@gmail.com>
2026-05-09 13:56:07 -0500

    style: formatted font and image sizes

--- src/pages/ArtworkDetails.tsx
+++ src/pages/ArtworkDetails.tsx
@@ -1,21 +1,32 @@
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import { artworks } from '../data/artworks'
 
 const ArtworkDetails = () => {
   const { id } = useParams()
+  const navigate = useNavigate()
   const artwork = artworks.find((a) => a.id === id)
 
   if (!artwork) return <div>Artwork not found</div>
 
   return (
-    <div className='m-auto text-gray-500 space-y-3 pb-10'>
-      <img src={artwork.src} alt={artwork.title} />
-      <div className='flex gap-10'>
-        <h1 className='text-gray-800'>{artwork.title}</h1>
-        <p className='text-gray-500'>{artwork.year}</p>
+    <div className='animate-fade-in m-auto px-4 pb-10'>
+      <div className='relative flex items-center justify-center text-gray-500 mb-6'>
+        <button
+          className='absolute left-0 hover:text-gray-800 hover:cursor-pointer transition-colors duration-200'
+          onClick={() => navigate(`/${artwork.category}`)}
+        >
+          BACK
+        </button>
       </div>
-      <p>{artwork.materials}</p>
-      <p className='text-gray-800'>{artwork.description}</p>
+      <div className='flex gap-6'>
+        <img src={artwork.src} alt={artwork.title} className='max-w-3xl' />
+        <div className='flex justify-end flex-col text-gray-500 italic'>
+          <h1 className='text-gray-800 text-lg'>{artwork.title}</h1>
+          <p>{artwork.year}</p>
+          <p>{artwork.materials}</p>
+          <p>{artwork.description}</p>
+        </div>
+      </div>
     </div>
   )
 }
